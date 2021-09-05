f0ff0aa12c17242806d901a3c1ee845cc0090226
stacon <constantinos-@hotmail.com>
2021-09-05 14:22:48 +0300

    Added Github pages deployment

--- src/components/layout/components/info/Info.js
+++ src/components/layout/components/info/Info.js
@@ -3,11 +3,15 @@ import { Grid, Typography } from "@material-ui/core";
 const Info = () => (
   <Grid item>
     <Typography variant="overline">
-      <a href="https://github.com/stacon/chess-concept" target="_blank">
+      <a
+        href="https://github.com/stacon/chess-concept"
+        target="_blank"
+        rel="noreferrer"
+      >
         One Pawn Chess concept
       </a>{" "}
       created by{" "}
-      <a href="https://github.com/stacon" target="_blank">
+      <a href="https://github.com/stacon" target="_blank" rel="noreferrer">
         stacon
       </a>
     </Typography>
